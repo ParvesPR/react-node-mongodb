c9a912f5d2019f824c05270b7d6bd534a910ce21
ParvesPR <parves2585@gmail.com>
2022-04-22 14:11:51 +0600

    data send to server side

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -5,15 +5,28 @@ const AddUser = () => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
-        const user = (name, email);
+        const user = {name, email};
+
+        // SEND DATA TO SERVER
+        fetch('http://localhost:5000/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data =>
+                console.log('success', data)
+            )
     }
 
     return (
         <div>
             <h2>Please add a user</h2>
             <form onSubmit={handleAddUser}>
-                <input type="text" placeholder='Name' required /> <br />
-                <input type="email" placeholder='Email' required /> <br />
+                <input type="text" name='name' placeholder='Name' required /> <br />
+                <input type="email" name='email' placeholder='Email' required /> <br />
                 <input type="submit" value="Add User" />
             </form>
         </div>
