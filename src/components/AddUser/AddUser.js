2349e236a9254375063a215a31386a6e33727210
ParvesPR <parves2585@gmail.com>
2022-04-22 13:40:05 +0600

    input form added

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const Adduser = () => {
+const AddUser = () => {
+    const handleAddUser = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = (name, email);
+    }
+
     return (
         <div>
             <h2>Please add a user</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" placeholder='Name' required /> <br />
+                <input type="email" placeholder='Email' required /> <br />
+                <input type="submit" value="Add User" />
+            </form>
         </div>
     );
 };
 
-export default Adduser;
+export default AddUser;
