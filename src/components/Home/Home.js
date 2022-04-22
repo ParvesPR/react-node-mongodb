4d4374fc589d78ce0a75ea2560c58d9b4dec8bec
ParvesPR <parves2585@gmail.com>
2022-04-22 19:51:09 +0600

    user delete function added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,12 +6,22 @@ const Home = () => {
         fetch('http://localhost:5000/user')
             .then(res => res.json())
             .then(data => setUsers(data))
-    }, [])
+    }, []);
+
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Are you sure you want to Delete?');
+        if (proceed) {
+            console.log('user deleting with id:', id)
+        }
+    }
     return (
         <div>
             <h2>Available Users: {users.length}</h2>
             {
-                users.map(user => <li>{user.name}:: {user.email}</li>)
+                users.map(user => <li key={user._id}>{user.name}:: {user.email}
+                    <button onClick={() => handleDeleteUser(user._id)}>X</button>
+                </li>
+                )
             }
         </div>
     );
