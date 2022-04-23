159a655c8226e34d021129b15aba6ff62880206b
ParvesPR <parves2585@gmail.com>
2022-04-23 09:59:37 +0600

    user deleted from ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,19 @@ const Home = () => {
     const handleDeleteUser = id => {
         const proceed = window.confirm('Are you sure you want to Delete?');
         if (proceed) {
-            console.log('user deleting with id:', id)
+            console.log('user deleting with id:', id);
+            const url = `http://localhost:5000/user/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('Successfully deleted!!');
+                        const remainingUser = users.filter(user => user._id !== id)
+                        setUsers(remainingUser);
+                    }
+                })
         }
     }
     return (
